3384ef03097b13731322d6eb4c0c9b808857a12a
MelkCampos <melkcampos33@gmail.com>
2020-02-06 22:52:18 -0300

    game

--- game.js
+++ game.js
@@ -2,7 +2,8 @@ var state = {
 
     // That's where will load the images and sounds
     preload: function() {
-        Game.load.image('cube', 'sprites/cube.png')
+        Game.load.image('cube', 'sprites/pccc.png')
+        Game.load.image('pipe', 'sprites/block.jpg')
     },
 
 
@@ -25,6 +26,12 @@ var state = {
         var spaceKey = Game.input.keyboard.addKey( Phaser.Keyboard.SPACEBAR )
 
         spaceKey.onDown.add(this.fly, this)
+
+
+        // PIPES
+        this.pipes = Game.add.group()
+
+        this.timer = Game.time.events.loop(1500, this.addRowOfPiper, this)
     },
 
 
@@ -53,6 +60,41 @@ var state = {
 
         // start the 'state'
         Game.state.start('state')
+    },
+
+    // add the pipes in the game
+    addOnePipe: function(pointX, pointY) {
+
+        // Create a pipe at the position 'x' and 'y' 
+        var pipe = Game.add.sprite(pointX, pointY, 'pipe')
+
+        // Add the pipe to our previously created group
+        this.pipes.add(pipe)
+
+        // Enable physics on the pipe
+        Game.physics.arcade.enable(pipe)
+
+        // Add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200
+
+        // Automatically kill the pipe when it's no longer visible
+        pipe.checkWorldBounds = true
+        pipe.outOfBoundsKill = true
+    },
+
+    addRowOfPiper: function() {
+
+        // Randomly pick a number between 1 and 5
+        // This will be the hole position
+
+        var hole = Math.floor(Math.random() * 5) + 1
+
+        for(let i = 0; i < 8; i++) {
+
+            if (i != hole && i != hole + 1)
+            this.addOnePipe(400, i * 60 + 10)
+        }
+
     }
 
 }
